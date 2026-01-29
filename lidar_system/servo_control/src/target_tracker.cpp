a4add7acdacf7c54c80f0bbe0150169e4f1aeee2
ty-runner <ty.runner02@gmail.com>
2026-01-29 10:49:34 -0800

    target tracker math implemented

--- lidar_system/servo_control/src/target_tracker.cpp
+++ lidar_system/servo_control/src/target_tracker.cpp
@@ -115,6 +115,14 @@ void TargetTrackerNode::angle_transform(float* pan_tilt_angles, float* drone_pos
     float zd_ = object_position[2] - drone_position[2]; // diff in z
 
     float horizontal_angle = atan2(yd_, xd_) * (180 / PI); //degrees
+
+    std::cout << "THETA: " << horizontal_angle << std::endl;
+    
+    float magnitude = std::sqrt(xd_ * xd_ + yd_ * yd_ + zd_ * zd_);
+
+    float vertical_angle = std::clamp(asin(zd_ / magnitude) * (180 / PI), -45.0f, 135.0f);
+
+    std::cout << "PHI: " << vertical_angle << std::endl;
 }
 
 int main(int argc, char **argv)
